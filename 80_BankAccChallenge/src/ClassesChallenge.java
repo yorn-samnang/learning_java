92353d9d94a4d915d35e058e9e5a03a46810fa71
Samnang <samnanglove123123@gmail.com>
2025-10-27 09:14:50 +0700

    modified the challenge

--- 80_BankAccChallenge/src/ClassesChallenge.java
+++ 80_BankAccChallenge/src/ClassesChallenge.java
@@ -2,9 +2,23 @@ public class ClassesChallenge {
     public static void main(String[] args) {
 //        BankAccount bankAccount = new BankAccount(); // constructor
 
-        BankAccount jameAcc = new BankAccount(1, 10000, "Jame", "jame@gmail.com", "086589019");
+        BankAccount jameAcc = new BankAccount(
+                1,
+                10000,
+                "Jame",
+                "jame@gmail.com",
+                "086589019"
+        );
 
-        System.out.println(jameAcc.getAccountNumber());
+        BankAccount nangAcc = new BankAccount(
+                2,
+                100001,
+                "Nang",
+                "nang@gmail.com",
+                "0865890190"
+        );
+
+//        System.out.println(jameAcc.getAccountNumber());
 
 
 //        bankAccount.setAccountName("Jame");
@@ -16,12 +30,29 @@ public class ClassesChallenge {
 //        System.out.println(bankAccount.deposingFunds(1000));
 //        System.out.println(BankAccount.withdrawFunds(2000));
 //        System.out.println(bankAccount.deposingFunds(1000));
-        // Print account info
-        System.out.println("Account Info:");
-        System.out.println("Number: " + jameAcc.getAccountNumber());
-        System.out.println("Name: " + jameAcc.getAccountName());
-        System.out.println("Balance: $" + jameAcc.getBalance());
-        System.out.println(jameAcc.deposingFunds(1000));
+//        // Print account info
+//        System.out.println("Account Info:");
+//        System.out.println("Number: " + jameAcc.getAccountNumber());
+//        System.out.println("Name: " + jameAcc.getAccountName());
+//        System.out.println("Balance: $" + jameAcc.getBalance());
+//        System.out.println(jameAcc.deposingFunds(1000));
+//
+//        // Print account info
+//        System.out.println("Account Info:");
+//        System.out.println("Number: " + nangAcc.getAccountNumber());
+//        System.out.println("Name: " + nangAcc.getAccountName());
+//        System.out.println("Balance: $" + nangAcc.getBalance());
+//        System.out.println(nangAcc.deposingFunds(1000));
+        printAccount(jameAcc);
 
+    }
+
+    public static void printAccount(BankAccount account) {
+        System.out.println(account.getAccountNumber());
+        System.out.println("Account Info:");
+        System.out.println("Number: " + account.getAccountNumber());
+        System.out.println("Name: " + account.getAccountName());
+        System.out.println("Balance: $" + account.getBalance());
+        System.out.println(account.deposingFunds(1000));
     }
 }
